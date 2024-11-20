bace3b0037055038ae03488495f651173df02278
Sunil Sharma <76843848+santmailo@users.noreply.github.com>
2024-11-20 19:55:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,23 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+ let decodedArr = []; // Array to hold the decoded characters
+  
+  // Loop through each character of the input string
+  for (let i = 0; i < encodedStr.length; i++) {
+    let char = encodedStr[i];
+    
+    // If the character is in the lookup object, decode it
+    if (lookup[char]) {
+      decodedArr.push(lookup[char]);
+    } else {
+      // If it's not in the lookup object, push it as is (non-alphabetic characters)
+      decodedArr.push(char);
+    }
+  }
 
-  return ;//return decodedArr
+  // Join the array into a string and return it
+  return decodedArr.join('');
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
